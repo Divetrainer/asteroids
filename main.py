b567ea921b4290e1ae54fa4582f509cdb1180542
Divetrainer <ian@divelab.tech>
2025-08-13 22:38:27 -0700

    movement of asteroids and collision detection

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import sys
 import pygame
 from constants import *
 from circleshape import CircleShape
 from player import *
+from asteroidfield import *
+from asteroid import *
 
 def main():
     pygame.init()
@@ -10,21 +13,29 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
-    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+    asteroidfield = AsteroidField()
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over")
+                sys.exit()
         screen.fill("black")
         for art in drawable:
             art.draw(screen)
-        updatable.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     
